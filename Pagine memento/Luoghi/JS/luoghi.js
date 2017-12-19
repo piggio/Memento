1f051c1117e35373732f9ceacfb172fe6f763cf6
Dario Salle <dario.salle@gmail.com>
2017-12-19 15:29:33 +0100

    Leaflet.js implementato

---
+++ Pagine memento/Luoghi/JS/luoghi.js
@@ -0,0 +1,8 @@
+var myMap = L.map('mapSearch').setView([41.886191, 12.489877], 6);
+
+L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'your.mapbox.access.token'
+}).addTo(myMap);
