335e89c7761e411ada1e3deeefa6ed20eba255b8
piggio <leonardo.alfonsi@iclud.com>
2017-12-19 15:56:39 +0100

    add handlebars

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,36 @@
-$(document).ready(function() {
-    ;
+$.getJSON( "Json/data.json", function( data ) {
+  
+   var storie = data.stories;
+    
+    console.log(storie);
+    console.log(document.getElementById("entry-template"));
+ var source   = document.getElementById("entry-template").innerHTML;
+var template = Handlebars.compile(source);
+    
+    console.log(data);
+    
+    
+    for (var i=0; i< storie.length; i++) {
+        
+        console.log(i);
+        var context = storie[i];
+        
+        
+        
+        
+        var html = template(context);
+        
+        console.log(html);
+        $('#cards').append(html);
+        
+        
+        
+    }
+    
+    
+    
 });
+
+
 
 
